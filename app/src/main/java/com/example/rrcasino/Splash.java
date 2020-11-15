f5da1c34fd8392cc76ecab9eec8c813288d69149
rayala3314 <rayala19@csub.edu>
2020-11-14 21:32:48 -0800

    Added splashscreen for app startup

---
+++ app/src/main/java/com/example/rrcasino/Splash.java
@@ -0,0 +1,26 @@
+package com.example.rrcasino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
