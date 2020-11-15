42c64e279841cb30f75c6c9eed9d87315b94793b
kemenike <kanayoemenike37@gmail.com>
2020-11-15 04:34:05 -0800

    Updated with classes and come functions implemented

--- app/src/main/java/com/example/rrcasino/DeckHandler.java
+++ app/src/main/java/com/example/rrcasino/DeckHandler.java
@@ -1,4 +1,42 @@
 package com.example.rrcasino;
+//Created By Kanayo Emenike
+/*Description: This file will handle all card based inquiries.
+  This includes:
+    - Creating the cards and decks.
+    - Shuffling the cards.
+    - Maintaining the used cards in the decks.
+ */
+public class DeckHandler
+{
+    public class Card
+    {
+        int suit, value, discarded;
+        //If the card is discarded, discarded is 1 or greater.
+        public void create()
+        {
+            value = (int)Math.random() * (14-1+1)+1;
+            suit = (int)Math.random() * (5-1+1)+1;
+            discarded = 0;
+        }
+    }
+    public class Deck
+    {
+        Card deck[];
 
-public class DeckHandler {
+        public void create()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                deck[i] = new Card();
+            }
+        }
+
+
+    }
+    public class Shoe
+    {
+
+    }
+
+
 }
