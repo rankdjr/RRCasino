1de6519d2eece65325a4a7c8ac9b52c1d4d8091f
Doug Rank <drank@csub.edu>
2020-11-18 18:14:16 -0800

    update deckhandler class: added constructors to generate deck and shoe; added shuffle and card deal functions;

--- app/src/main/java/com/example/rrcasino/DeckHandler.java
+++ app/src/main/java/com/example/rrcasino/DeckHandler.java
@@ -1,4 +1,10 @@
 package com.example.rrcasino;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+import java.util.ArrayList;
+
 //Created By Kanayo Emenike
 /*Description: This file will handle all card based inquiries.
   This includes:
@@ -10,7 +16,9 @@ public class DeckHandler
 {
     public class Card
     {
+        /*
         int suit, value, discarded;
+
         //If the card is discarded, discarded is 1 or greater.
         public void create()
         {
@@ -18,24 +26,118 @@ public class DeckHandler
             suit = (int)Math.random() * (5-1+1)+1;
             discarded = 0;
         }
+        */
+
+        // *********** Doug's additions start here *********************
+        int suit;
+        int rank;
+        public Card(int suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+
     }
     public class Deck
     {
+        /*
         Card deck[];
 
         public void create()
         {
             for (int i = 0; i < 52; i++)
             {
-                deck[i] = new Card();
+                //deck[i] = new Card();
+            }
+        }
+        */
+
+        // *********** Doug's additions start here *********************
+        private ArrayList<Card> cards;
+
+        public Deck()
+        {
+            cards = new ArrayList<>();
+            generateDeck();
+            shuffle();
+        }
+
+        public void generateDeck()
+        {
+            //while loop iterates 52 times, the iterator is incremented on the innermost for loop
+            //generating the deck creates and ordered deck in descending order from ace to 2
+            int i = 0;
+            while (i < 52) {
+                for (int suit = 0; suit < 4; suit++) {
+                    for (int rank = 0; rank < 13; rank++) {
+                        cards.add(new Card(suit, rank));
+                        i++;
+                    }
+                }
             }
         }
 
+        public void shuffle()
+        {
+            Collections.shuffle(cards);
+        }
 
+        public Card getCard()
+        {
+            return cards.remove(0);
+        }
+
+        /*
+        public int getNumberOfCards()
+        {
+            return cards.size();
+        }
+        */
     }
+
     public class Shoe
     {
+        private ArrayList<Card> shoe;
+        private int numOfDecks;
 
+        public Shoe()
+        {
+            numOfDecks = 8;
+            shoe = new ArrayList<>();
+            generateShoe();
+        }
+
+        public void generateShoe()
+        {
+            //while loop iterates 52 times with a repetition based on the number of decks wanted in the shoe
+            //the iterator is incremented on the innermost for loop
+            //generating the shoe creates an ordered stack of decks in descending order from ace to 2
+            int i = 0;
+            while (i < 52 * numOfDecks) {
+                for (int suit = 0; suit < 4; suit++) {
+                    for (int rank = 0; rank < 13; rank++) {
+                        shoe.add(new Card(suit, rank));
+                        i++;
+                    }
+                }
+            }
+        }
+
+        public void shuffle()
+        {
+            Collections.shuffle(shoe);
+        }
+
+        public Card getCard()
+        {
+            return shoe.remove(0);
+        }
+
+        public int getNumOfCards()
+        {
+            return shoe.size();
+        }
     }
 
 
--- app/src/main/java/com/example/rrcasino/gameBlackJack.java
+++ app/src/main/java/com/example/rrcasino/gameBlackJack.java
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class gameBlackJack extends AppCompatActivity {
+    /*
     Card[] shoe;
+    int numOfDecks = 1;
     int k = 0;
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_black_jack);
 
+    }
+
+    /*
+    public void createDeck () {
         // generate shoe
-        int numOfDecks = 1;
-        shoe = new Card[52*numOfDecks];
+        // creates a shoe of decks by looping through array and assigning
+        // value of cards in ascending order
+        shoe = new Card[52 * numOfDecks];
         for (int i = 0; i < numOfDecks; i++) {
             for (int suit = 0; suit < 4; suit++) {
                 for (int rank = 0; rank < 13; rank++) {
@@ -25,4 +36,25 @@ public class gameBlackJack extends AppCompatActivity {
             }
         }
     }
+
+    public void shuffleDeck () {
+        // shuffle 100 times
+        // inner loop shuffles cards in array by swapping elements
+        // outer loop runs shuffle 100 times
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < k; j++) {
+                Random r = new Random();
+                int rand = r.nextInt(k + 1);
+                Card temp = shoe[j];
+                shoe[j] = shoe[rand];
+                shoe[rand] = temp;
+            }
+        }
+    }
+
+    void dealCard () {
+
+    }
+    */
+
 }
