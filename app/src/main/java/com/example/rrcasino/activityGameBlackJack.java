1cf596e6dcb39e6eaf2d3a41803037a06840f10f
kemenike <kanayoemenike37@gmail.com>
2020-12-02 09:50:25 -0800

    -Changed themes.xml to represent CSUB official colors. -Finalized Help main menu and functionality. -Poker and Blackjack help menu UI completed. -Changed Manifest, gameSelection.java, helpMenu.java, MainActivity.java to remove the title throughout the app. -Fixed some various type casting redundancy.

--- app/src/main/java/com/example/rrcasino/activityGameBlackJack.java
+++ app/src/main/java/com/example/rrcasino/activityGameBlackJack.java
@@ -1,6 +1,6 @@
 package com.example.rrcasino;
 
-/**
+/*
  * Created by Doug
  * Main activity file
  */
@@ -79,6 +79,7 @@ public class activityGameBlackJack extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_black_jack);
+        this.getSupportActionBar().hide();
 
         // Assign TextView IDs from XML
         tvPlayerScore = findViewById(R.id.playerScore);
---
+++ app/src/main/java/com/example/rrcasino/pokerHelp.java
@@ -0,0 +1,15 @@
+package com.example.rrcasino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pokerHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poker_help);
+        this.getSupportActionBar().hide();
+    }
+}
