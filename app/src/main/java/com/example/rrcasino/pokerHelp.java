e36d74672cb697f4395682a096fbe599bb7c1325
kemenike <kanayoemenike37@gmail.com>
2020-12-02 11:28:26 -0800

    Sync with master branch

--- app/src/main/java/com/example/rrcasino/pokerHelp.java
+++ app/src/main/java/com/example/rrcasino/pokerHelp.java
@@ -3,13 +3,28 @@ package com.example.rrcasino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class pokerHelp extends AppCompatActivity {
+
+    private Button overview;
+    private Button actions;
+    private Button howtowin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poker_help);
         this.getSupportActionBar().hide();
+
+        overview.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
